6e3c7b92e8bbce4f480feb4d1e8201c510af1c65
boredwookie <rion.carter@rubion.com>
2018-10-15 06:44:17 -0600

    Fixed recursion issue

--- eme-ash.sh
+++ eme-ash.sh
@@ -1,36 +1,60 @@
 #!/bin/sh
+# Licensed for use under Apache 2.0
+#
+# ec2 metadata extractor (eme.sh) for almquist shell (ASH)
+#   v0.2
 #
 # This version works in the ASH shell provided by busybox
+#
+#   To disable 'bolded' output headers, pass in the '-noformat' flag
+#
+
+if [ $1 == "-noformat" ]; then
+  outputformatting=false
+else
+  outputformatting=true
+fi
+
 
 metadataroot=http://169.254.169.254/latest
-
 function walk(){
-  root=$1 # Takes the next path to 'walk' via wget
-
-  sublisting=$(wget -O- -q $root)
-
+  local root=$1 # Takes the next path to 'walk' via wget
+  #echo "DEBUG: WALK: wgetting: $root"
+  sublisting=$(wget -O- -q $root 2>/dev/null)
+  sublisting="$sublisting
+"
+  #echo "==DEBUG: start sublisting=="
+  #echo "$sublisting"
+  #echo "==DEBUG: done sublisting=="
   for li in $sublisting; do
-    if [[ "$li" == */  ]]; then
+    #
+    # [[ "${li: -1}" == "/"  ]] is a little more explicit about checking if the last character is a slash
+    #
+    if [[ "${li: -1}" == "/"  ]]; then
+      #echo "DEBUG: WALK: enumerating $root$li"
       walk "$root$li"
     else
       printout "$li" "$root"
     fi
-  done  
+  done
 }
-
 function printout() {
-  entry=$1        # entry name
-  pathprefix=$2   # Shows where this item lives in the metadata tree
-
-  entrydetails=$(wget -O- -q $pathprefix$entry)
-
+  local entry=$1        # entry name
+  local pathprefix=$2   # Shows where this item lives in the metadata tree
+  #echo "DEBUG: PRINT: wgetting details: $pathprefix$entry"
+  entrydetails=$(wget -O- -q $pathprefix$entry 2>/dev/null)
   echo ""
   echo ""
-  echo -e "\033[1m$pathprefix$entry:\033[0m"
+  if [[ "$outputformatting" == "true" ]]; then
+    echo -e "\033[1m$pathprefix$entry:\033[0m"
+  else
+    # No formatting
+    echo -e "$pathprefix$entry:"
+  fi
   echo "$entrydetails"
 }
 
-walk "$metadataroot/"
+#walk "$metadataroot/"              # Doesn't work since amazon 'conveniently' leaves out the trailing slashes
 walk "$metadataroot/meta-data/"
-walk "$metadatarooti/dynamic/"
-walk "$metadatarooti/user-data/"
+walk "$metadataroot/dynamic/"
+walk "$metadataroot/user-data"
